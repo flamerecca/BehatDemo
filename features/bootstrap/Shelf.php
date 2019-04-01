1592e100aec640b817544027c3cb2822e46a8ca9
recca <flamerecca711@gmail.com>
2019-04-02 02:28:39 +0800

    feaT: Add features implementation

---
+++ features/bootstrap/Shelf.php
@@ -0,0 +1,17 @@
+<?php
+
+
+final class Shelf
+{
+    private $priceMap;
+
+    public function setProductPrice($product, $price)
+    {
+        $this->priceMap[$product] = $price;
+    }
+
+    public function getProductPrice($product)
+    {
+        return $this->priceMap[$product];
+    }
+}
